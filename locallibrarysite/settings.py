267ebd9bc1e3cd94676365492aec11a2344031e8
Bzan96 <39344747+Bzan96@users.noreply.github.com>
2018-07-14 12:18:22 +0200

    Update settings.py

--- locallibrarysite/settings.py
+++ locallibrarysite/settings.py
@@ -120,8 +120,9 @@ EMAIL_BACKEND =  'django.core.mail.backends.console.EmailBackend'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES["default"].update(db_from_env)
+DATABASES = { 'default': dj_database_url.config(conn_max_age=500) }
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES["default"].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
